632286d11bd6b30c471144c7ba411aaae4cb9bce
Simon Blee <simblee@gmail.com>
2013-06-16 22:35:56 +0800

    Syntax changes.

--- draggable.js
+++ draggable.js
@@ -1,12 +1,12 @@
 /**
- * Makes an element draggable within it's parent. 
- * 
- * @require jquery 1.7
+ * Makes an element draggable within it's parent.
+ *
+ * @require jquery
  */
 
-(function( $ ){
+(function ($) {
 
-	var Draggable = function( o, element ){
+	var Draggable = function (o, element) {
 		this.element = element;
 		this.move = false;
 		this.prevX = null;
@@ -26,19 +26,19 @@
 			lock: 'none',
 			onMove: function () {}
 		}
-		
+
 		// Extend the options
 		$.extend( this.options, o );
 
 		// Init the plugin
 		this.init();
-	};    
-	
+	};
+
 	Draggable.prototype = {
 
 		constructor: Draggable,
 
-		init : function () {
+		init: function () {
 			// Set to absolute positioning and parent to relative
 			this.element.css( 'position', 'absolute');
 
@@ -54,7 +54,7 @@
 		},
 
 		// Prevent the object from being dragged in a particular direction
-		lock: function ( direction ) {
+		lock: function (direction) {
 			if ( direction === 'horizontal' ) {
 				this.moveHorizontal = false;
 				this.moveVertical = true;
@@ -69,8 +69,8 @@
 
 		// Disable by removing events
 		disable: function () {
-			this.element.off( 'mousedown.draggable' );
-			$(document).trigger( 'mouseup.draggable' );
+			this.element.off('mousedown.draggable');
+			$(document).trigger('mouseup.draggable');
 		},
 
 		// Re-bind the events to enable
@@ -82,7 +82,7 @@
 			var self = this;
 
 			// On mouse down, START moving the element
-			this.element.on( 'mousedown.draggable', function ( event ) {
+			this.element.on('mousedown.draggable', function (event) {
 				// Set movable
 				self.move = true;
 				$(this).addClass('drag');
@@ -97,37 +97,37 @@
 
 				// Bind and trigger the mouse move event to calculate previous position
 				// Use document as mouse will most likely move out of element during draggable
-				$(document).on( 'mousemove.draggable', function ( event ) {
-					if( self.move ){
-						self.onMouseMove( event );
+				$(document).on( 'mousemove.draggable', function (event) {
+					if (self.move) {
+						self.onMouseMove(event);
 					}
 				});
 
 				// On mouse up, STOP moving the element
-				$(document).on( 'mouseup.draggable', function () {
+				$(document).on('mouseup.draggable', function () {
 					self.move = false;
 					self.element.removeClass('drag');
 				});
 
-				//Prevent default 
+				//Prevent default
 				event.preventDefault();
 			});
 		},
 
 		// Move the element by the difference in mouse move from last move
-		onMouseMove: function ( event ) {
+		onMouseMove: function (event) {
 			var pos = this.element.position(),
 				difX = event.pageX - this.prevX,
-				difY = event.pageY - this.prevY;	
+				difY = event.pageY - this.prevY;
 
 			// Move the element the difference of the previous and current mouse position
-			if( this.prevX !== null ){
-				if( this.moveVertical ){
-					this.moveElement( 'top', pos.top + difY, event );
+			if (this.prevX !== null) {
+				if (this.moveVertical) {
+					this.moveElement('top', pos.top + difY, event);
 				}
 
-				if( this.moveHorizontal ){
-					this.moveElement( 'left', pos.left + difX, event );
+				if (this.moveHorizontal) {
+					this.moveElement('left', pos.left + difX, event);
 				}
 			}
 
@@ -139,47 +139,47 @@
 		// Move an element along a particular dimension. This method can be called
 		// externally to move the draggable element. When calling externally, leave
 		// 'event' undefined.
-		moveElement: function ( dim, val, event ) {
-			var move = ( 0 <= val && val <= this.getMaxBounds( dim ) );
+		moveElement: function (dim, val, event) {
+			var move = (0 <= val && val <= this.getMaxBounds(dim));
 
 			// If we have an event (must be mouse move) check if
 			// mouse is out of bounds
-			if( typeof event !== 'undefined' ){
-				move = this.mouseInClickBounds( dim, event );
+			if (typeof event !== 'undefined') {
+				move = this.mouseInClickBounds(dim, event);
 			}
 
 			//Set position
-			if( typeof move === 'number' ) {
+			if (typeof move === 'number') {
 				// If 'move' is -1 or val < 0, must be at lower bound
-				val = ( move === -1 || val < 0 ) ? 0 : this.getMaxBounds( dim );
-			} else if( move == false ){
+				val = (move === -1 || val < 0) ? 0 : this.getMaxBounds(dim);
+			} else if (move == false) {
 				// Set to one of the bounds
-				val = ( val < 0 ) ? 0 : this.getMaxBounds( dim );
+				val = (val < 0) ? 0 : this.getMaxBounds(dim);
 			}
-			this.element.css( dim, val + 'px' );
+			this.element.css(dim, val + 'px');
 
 			// Call the callback - set element as 'this', pass in direction
-			this.options.onMove.call( this.element );
+			this.options.onMove.call(this.element);
 		},
 
 		// Returns true if the mouse is in the bound of both the parent
-		// and the click position of the element. This causes the element 
-		// to not move if the mouse is outside the parent AND if the mouse 
+		// and the click position of the element. This causes the element
+		// to not move if the mouse is outside the parent AND if the mouse
 		// is outside its original click position. The click coordinates
 		// add an offset to the parent element, acting like a padding equal
 		// to the x and y position of the click inside the element. This means
-		// the element can be moved only when the mouse gets back to the same 
+		// the element can be moved only when the mouse gets back to the same
 		// position it was clicked on, not before
-		mouseInClickBounds: function ( dim, event ) {
+		mouseInClickBounds: function (dim, event) {
 			var os = this.element.parent().offset(),
 				clickDim = dim === 'top' ? this.clickY : this.clickX,
 				eventDim = dim === 'top' ? event.pageY : event.pageX;
 
 			// If element not bound, mouse always in bounds
-			if( this.options.bound ){
-				if( ( os[dim] + clickDim ) <= eventDim && eventDim <= ( os[dim] + this.getMaxBounds( dim ) + clickDim ) ){
+			if (this.options.bound) {
+				if ((os[dim] + clickDim) <= eventDim && eventDim <= (os[dim] + this.getMaxBounds(dim) + clickDim)) {
 					return true;
-				} else if( eventDim < ( os[dim] + clickDim ) ){
+				} else if (eventDim < (os[dim] + clickDim)) {
 					return -1;
 				} else {
 					return 1;
@@ -190,35 +190,35 @@
 		},
 
 		// Calculate the max bounds of the element
-		getMaxBounds: function ( dim ) {
-			if( dim === 'top' ){
-				return this.element.parent().outerHeight() - this.element.outerHeight( true );
+		getMaxBounds: function (dim) {
+			if (dim === 'top') {
+				return this.element.parent().outerHeight() - this.element.outerHeight(true);
 			} else {
-				return this.element.parent().outerWidth() - this.element.outerWidth( true );
+				return this.element.parent().outerWidth() - this.element.outerWidth(true);
 			}
 		}
 	};
-	
-	
-	$.fn.draggable = function( method ) {
+
+
+	$.fn.draggable = function (method) {
 		var ret,
 			data,
 			args = arguments;
 
-		this.each(function(){
+		this.each(function () {
 			// Grab the object or create if not exist
-			data = $(this).data( 'draggable' );            
-			if( !data ){
-				data = $(this).data( 'draggable', { 
-					o : new Draggable( method, $(this) ) 
-				}).data( 'draggable' );
+			data = $(this).data('draggable');
+			if (!data) {
+				data = $(this).data('draggable', {
+					o : new Draggable(method, $(this))
+				}).data('draggable');
 			}
 
 			// Call method if it exists
-			if ( data.o[method] ) {
-				ret = data.o[ method ].apply( data.o, Array.prototype.slice.call( args, 1 ));
+			if (data.o[method]) {
+				ret = data.o[method].apply(data.o, Array.prototype.slice.call(args, 1));
 			}
 			return typeof ret === 'undefined' ? $(this) : ret;
 		});
 	};
-})( jQuery );
+})(jQuery);
--- scroller.js
+++ scroller.js
@@ -1,19 +1,17 @@
 /**
- * Content Scroller. Waps a content area in a scroller, allowing the 
- * content to be scrolled and the scroll bar to be styled. Reason for 
+ * Content Scroller. Waps a content area in a scroller, allowing the
+ * content to be scrolled and the scroll bar to be styled. Reason for
  * making: to keep the file size very small.
  *
- * Author: Simon Blee (The Gift Mansion) (simblee@gmail.com, simon@thegiftmansion.com.au)
- *
- * @require jquery 1.7
+ * @require jquery
  * @require draggable.js
  * @require jquery.mousewheel.js (from Remy Sharp)
  * @require jquery.mousehold.js (from Brandon Aaron)
  */
 
-(function( $ ){
+(function ($) {
 
-    var Scroller = function( o, element ){ 
+    var Scroller = function(o, element) {
         this.element = element;
         this.parent = null;
         this.upBtn = null;
@@ -33,37 +31,38 @@
         };
 
         // Init the scroller
-        this.init( o );
-    };    
-    
+        this.init(o);
+    };
+
     Scroller.prototype = {
 
         constructor: Scroller,
 
-        init : function( o ){           
+        init: function(o) {
             var self = this;
 
             // Extend the input options
-            $.extend( this.options, o );
+            $.extend(this.options, o);
 
             //Set the size function to call from jQuery
             this.func = this.options.orientation === 'vertical' ? ['height', 'outerHeight'] : ['width', 'outerWidth'];
             this.dim = this.options.orientation === 'vertical' ? 'top' : 'left';
 
             // Wrap the element and add scroller template
-            this.element.addClass('scroll-content').css( 'position', 'relative'
-            ).css( this.dim, '0px'
-            ).wrap( '<div class="scrollable" />' 
-            ).parent().append(                
+            this.element.addClass('scroll-content').css('position', 'relative')
+            .css(this.dim, '0px')
+            .wrap('<div class="scrollable" />')
+            .parent()
+            .append(
                 '<div class="scroller">'+
                     '<div class="scroller-up scroller-up-down" direction="-1"></div>'+
                     '<div class="scroller-handle-wrap">'+
                         '<div class="scroller-handle"></div>'+
                     '</div>'+
                     '<div class="scroller-down scroller-up-down" direction="1"></div>'+
-                '</div>'
-            // ).css( 'position', 'relative' 
-            ).addClass( this.options.orientation );
+                '</div>')
+            // .css('position', 'relative')
+            .addClass(this.options.orientation);
 
             // Create quick reference elements
             this.parent = this.element.parent();
@@ -72,7 +71,7 @@
             this.handle = this.parent.find(".scroller-handle");
 
             // Show scroll buttons?
-            if( !this.options.showButtons ){
+            if (! this.options.showButtons) {
                 this.upBtn.hide();
                 this.downBtn.hide();
             }
@@ -82,7 +81,7 @@
                 bound: true,
                 lock: this.options.orientation === 'vertical' ? 'horizontal' : 'vertical',
                 onMove: function () {
-                    if ( self.element[ self.func[0] ]() > self.parent[ self.func[0] ]() ) {
+                    if (self.element[self.func[0]]() > self.parent[self.func[0]]()) {
                         self.scrollContent();
                     }
                 }
@@ -99,148 +98,148 @@
                 handleSize = 0;
 
             // Calculate the handle size
-            if( this.parent[ this.func[0] ]() < this.element[ this.func[0] ]() ){
-                handleSize = this.parent[ this.func[0] ]() * ( this.parent[ this.func[0] ]() / this.element[ this.func[0] ]() ); 
-                handleSize = this.upBtn.is(':visible') ? handleSize - ( this.downBtn[ this.func[1] ]() + this.upBtn[ this.func[1] ]() ) : handleSize;
+            if(this.parent[this.func[0]]() < this.element[this.func[0]]()) {
+                handleSize = this.parent[this.func[0]]() * (this.parent[this.func[0]]() / this.element[this.func[0]]());
+                handleSize = this.upBtn.is(':visible') ? handleSize - (this.downBtn[this.func[1]]() + this.upBtn[this.func[1]]()) : handleSize;
                 handleSize = handleSize < this.options.minHandleSize ? this.options.minHandleSize : handleSize;
             }
 
             //Set the handle size and wrap size
-            this.handle[ this.func[0] ]( handleSize );
-            handleWrapSize = this.upBtn.is(':visible') ? this.parent[ this.func[0] ]() - ( 2 * this.upBtn[ this.func[1] ]() ) : this.parent[ this.func[0] ]();
-            this.handle.parent()[  this.func[0]  ]( handleWrapSize );
+            this.handle[this.func[0]](handleSize);
+            handleWrapSize = this.upBtn.is(':visible') ? this.parent[this.func[0]]() - (2 * this.upBtn[this.func[1]]()) : this.parent[this.func[0]]();
+            this.handle.parent()[this.func[0]](handleWrapSize);
         },
 
         // Adjust scroller to match content size
-        reflowContent : function () {
+        reflowContent: function () {
             var newScroll,
-                currentScroll = parseFloat( this.element.css( this.dim ).replace('px', '') );
+                currentScroll = parseFloat(this.element.css(this.dim).replace('px', ''));
 
             // First resize the scroller
             this.sizeScrollerHandle();
 
-            if( this.element[ this.func[0] ]() < this.parent[ this.func[0] ]() ){
+            if(this.element[this.func[0]]() < this.parent[this.func[0]]()){
                 newScroll = 0;
-            } else if( currentScroll + this.element[ this.func[0] ]() < this.parent[ this.func[0] ]() ){
-                newScroll = this.parent[ this.func[0] ]() - this.element[ this.func[0] ]();
+            } else if(currentScroll + this.element[this.func[0]]() < this.parent[this.func[0]]()){
+                newScroll = this.parent[this.func[0]]() - this.element[this.func[0]]();
             } else {
                 newScroll = currentScroll *
-                            ( this.handle.parent()[ this.func[0] ]() - this.handle[ this.func[0] ]() ) /
-                            ( this.element[ this.func[0] ]() - this.parent[ this.func[0] ]() );
-            }  
+                            (this.handle.parent()[this.func[0]]() - this.handle[this.func[0]]()) /
+                            (this.element[this.func[0]]() - this.parent[this.func[0]]());
+            }
 
-            this.element.css( this.dim, newScroll + 'px' );          
+            this.element.css(this.dim, newScroll + 'px');
         },
 
         // Bind all the events to move the scroller
-        bindEvents : function () {
+        bindEvents: function () {
             var self = this;
 
             // Move the scroller with the mousewheel using the event helper
-            this.parent.on('mousewheel.scroller', function ( event, delta, deltaX, deltaY ) {
-                delta = self.options.orientation === 'vertical' ? deltaY : deltaX;                
-                self.scrollHandle( -1 * delta );
+            this.parent.on('mousewheel.scroller', function (event, delta, deltaX, deltaY) {
+                delta = self.options.orientation === 'vertical' ? deltaY : deltaX;
+                self.scrollHandle(-1 * delta);
                 event.preventDefault();
-                clearTimeout( self.enterTimeout );
+                clearTimeout(self.enterTimeout);
                 setEnterTimeout();
             });
 
             // When clicking, ensure the mouseenter class is set for the duration
-            this.handle.on( 'mousedown.scroller', function ( event ) {
-                clearTimeout( self.enterTimeout );
+            this.handle.on('mousedown.scroller', function (event) {
+                clearTimeout(self.enterTimeout);
                 self.handle.addClass('mouseenter');
             });
 
             $(document).on('mouseup.scroller', function () {
-                if( self.handle.hasClass('mouseenter') ){
+                if (self.handle.hasClass('mouseenter')) {
                     setEnterTimeout();
                 }
             });
 
             // Move the scroller with up/down clicks and mousehold
             this.parent.find('.scroller-up-down').on('mousedown.scroller', function () {
-                self.scrollHandle( parseInt( $(this).attr('direction'), 10 ) );
+                self.scrollHandle(parseInt($(this).attr('direction'), 10));
             });
 
-            if ( typeof $.mousehold === 'function' ){
-                this.parent.find('.scroller-up-down').mousehold( self.options.mouseholdTimeout, self.options.mouseholdDeadtime, function () {
-                    self.scrollHandle( parseInt( $(this).attr('direction'), 10 ) );
+            if (typeof $.mousehold === 'function') {
+                this.parent.find('.scroller-up-down').mousehold(self.options.mouseholdTimeout, self.options.mouseholdDeadtime, function () {
+                    self.scrollHandle(parseInt($(this).attr('direction'), 10));
                 });
             }
 
             // Clicking in the handle wrap should move handle to that location
-            this.handle.parent().on( 'click.scroller', function ( event ) {
+            this.handle.parent().on('click.scroller', function (event) {
                 var pos,
                     page = self.dim === 'top' ? event.pageY : event.pageX,
-                    clickPos = page - self.handle.parent().offset()[ self.dim ],
+                    clickPos = page - self.handle.parent().offset()[self.dim],
                     handlePos = self.handle.position()[self.dim];
 
                 // Make sure not over handle
-                if( clickPos < handlePos || ( handlePos + self.handle[ self.func[0] ]() ) < clickPos ){
-                    pos = clickPos < handlePos ? clickPos : clickPos - self.handle[ self.func[0] ]();
-                    self.handle.draggable( "moveElement", self.dim, pos );
+                if (clickPos < handlePos || (handlePos + self.handle[self.func[0]]()) < clickPos) {
+                    pos = clickPos < handlePos ? clickPos : clickPos - self.handle[self.func[0]]();
+                    self.handle.draggable("moveElement", self.dim, pos);
                 }
             });
 
             // Moving mouse into scroller adds 'mouseenter' class
-            this.parent.on('mouseenter.scroller', setEnterTimeout );
+            this.parent.on('mouseenter.scroller', setEnterTimeout);
 
-            function setEnterTimeout() {
+            function setEnterTimeout () {
                 self.handle.addClass('mouseenter');
-                self.enterTimeout = setTimeout( function () {
+                self.enterTimeout = setTimeout(function () {
                     self.handle.removeClass('mouseenter');
-                }, self.options.enterTimeoutDuration );
+                }, self.options.enterTimeoutDuration);
             }
         },
 
         // Move the scroller handle (events will need to call this)
-        scrollHandle : function( direction ){
+        scrollHandle : function (direction) {
             // Calculate the handle scroll value
-            var weight = this.options.scrollDistance * ( this.handle.parent()[ this.func[1] ]() - this.handle[ this.func[0] ]() ) / 
-                ( this.element[ this.func[0] ]() - this.parent[ this.func[0] ]() ),
-                diff = direction > 0 ? Math.ceil( direction * weight ) : Math.floor( direction * weight );
-            
+            var weight = this.options.scrollDistance * (this.handle.parent()[this.func[1]]() - this.handle[this.func[0]]()) /
+                (this.element[this.func[0]]() - this.parent[this.func[0]]()),
+                diff = direction > 0 ? Math.ceil(direction * weight) : Math.floor(direction * weight);
+
             // Move the scroll handle
-            this.handle.draggable( "moveElement", this.dim, this.handle.position()[ this.dim ] + diff );
+            this.handle.draggable("moveElement", this.dim, this.handle.position()[this.dim] + diff);
         },
 
         // Scroll the content
-        scrollContent : function(){
+        scrollContent : function () {
             // Calculate the element position
-            var val = - this.handle.position()[this.dim] * ( this.element[ this.func[1] ](true) - this.parent[ this.func[0] ]() ) / 
-                ( this.handle.parent()[ this.func[0] ]() - this.handle[ this.func[0] ]() );
+            var val = - this.handle.position()[this.dim] * (this.element[this.func[1]](true) - this.parent[this.func[0]]()) /
+                (this.handle.parent()[this.func[0]]() - this.handle[this.func[0]]());
 
             // Move the content by the specified scroll distance
-            if( val <= 0 && Math.abs( val ) + this.parent[ this.func[0] ]() < this.element[ this.func[0] ]() ){
-                this.element.css( this.dim, val + 'px' );
-            } else if( 0 < val ) {
-                this.element.css( this.dim, '0px' );
+            if (val <= 0 && Math.abs(val) + this.parent[this.func[0]]() < this.element[this.func[0]]()) {
+                this.element.css(this.dim, val + 'px');
+            } else if (0 < val) {
+                this.element.css(this.dim, '0px');
             } else {
-                this.element.css( this.dim, - ( this.element[ this.func[1] ](true) - this.parent[ this.func[0] ]() ) + 'px' );
+                this.element.css(this.dim, - (this.element[this.func[1]](true) - this.parent[this.func[0]]()) + 'px');
             }
         }
     };
-    
-    
-    $.fn.scroller = function( method ) {
+
+
+    $.fn.scroller = function (method) {
         var ret,
             data,
             args = arguments;
 
-        return this.each(function(){
+        return this.each(function () {
             // Grab the object or create if not exist
-            data = $(this).data( 'scroller' );            
-            if( !data ){
-                data = $(this).data( 'scroller', { 
-                    o : new Scroller( method, $(this) ) 
-                }).data( 'scroller' );
+            data = $(this).data('scroller');
+            if (!data) {
+                data = $(this).data('scroller', {
+                    o : new Scroller(method, $(this))
+                }).data('scroller');
             }
 
             // Call method if it exists
-            if ( data.o[method] ) {
-                ret = data.o[ method ].apply( data.o, Array.prototype.slice.call( args, 1 ) );
-            }  
-            return typeof ret === 'undefined' ? $(this) : ret;        
+            if (data.o[method]) {
+                ret = data.o[method].apply(data.o, Array.prototype.slice.call(args, 1));
+            }
+            return typeof ret === 'undefined' ? $(this) : ret;
         });
     };
-})( jQuery );
+})(jQuery);
